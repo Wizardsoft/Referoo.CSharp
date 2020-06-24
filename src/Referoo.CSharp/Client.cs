c438b0bf11bbd5dab0043afbcd2e43e391fb91aa
Ben Macpherson <ben@uniquecomputing.com.au>
2020-06-24 15:44:24 +1000

    Enough code to get CI/CD happening

---
+++ src/Referoo.CSharp/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Referoo.CSharp
+{
+    public partial class ReferooClient
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="baseUrl"></param>
+        public ReferooClient(string username, string password, string baseUrl = "https://api.messagemedia.com/v1/")
+        {
+            if (string.IsNullOrEmpty(username))
+                throw new Exception("Empty Username not Allowed");
+
+            if (string.IsNullOrEmpty(password))
+                throw new Exception("Empty Password not Allowed");
+
+            if (string.IsNullOrEmpty(baseUrl))
+                throw new Exception("Empty BaseUrl not Allowed");
+
+            Configuration.BaseUrl = baseUrl;
+            Configuration.Username = username;
+            Configuration.Password = password;
+        }
+
+        //public Numbers Numbers
+        //{
+        //    get { return Numbers.Instance; }
+        //}
+    }
+}
---
+++ src/Referoo.CSharp/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace Referoo.CSharp
+{
+    public class Configuration
+    {
+        public static string BaseUrl { get; set; }
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+    }
+}
