fc53121cae5853c2cfc5befed5a319be6f13f945
Ben Macpherson <ben@uniquecomputing.com.au>
2023-09-26 15:38:53 +1000

    Remove unneeded params

--- src/Referoo.NetStandard/Models/PutCandidateParameter.cs
+++ src/Referoo.NetStandard/Models/PutCandidateParameter.cs
@@ -13,52 +13,15 @@ namespace Referoo.NetStandard.Models
         [JsonProperty("email", Required = Required.Always)]
         public string Email { get; set; }
 
-        [JsonProperty("phone", Required = Required.AllowNull)]
-        public string Phone { get; set; }
-
-        [JsonProperty("notes", Required = Required.AllowNull)]
-        public string Notes { get; set; }
-
-        [JsonProperty("job", Required = Required.AllowNull)]
-        public string Job { get; set; }
-
-        [JsonProperty("company", Required = Required.AllowNull)]
-        public string Company { get; set; }
-
-        [JsonProperty("ip_address")]
-        public string IpAddress { get; set; }
-
         [JsonProperty("questionnairesNum", Required = Required.AllowNull)]
         public int QuestionnairesNum { get; set; }
-
-        [JsonProperty("brandsNum")]
-        public int? BrandsNum { get; set; }
 
         [JsonProperty("number_of_references_required", Required = Required.Always)]
         public int NumberOfReferencesRequired { get; set; }
 
-        [JsonProperty("one_click_reference", Required = Required.Always)]
-        public int OneClickReference { get; set; }
-
-        [JsonProperty("send_referee_email_reminders")]
-        public int SendRefereeEmailReminders { get; set; }
-
-        [JsonProperty("send_candidate_email_reminders")]
-        public int SendCandidateEmailReminders { get; set; }
-
-        [JsonProperty("do_not_contact")]
-        public int DoNotContact { get; set; }
-
-        [JsonProperty("do_not_contact_recruiter")]
-        public int DoNotContactRecruiter { get; set; }
-
-        [JsonProperty("candidate_locked")]
         public int CandidateLocked { get; set; }
 
         [JsonProperty("archived")]
         public int Archived { get; set; }
-
-        [JsonProperty("custom_data")]
-        public string CustomData { get; set; }
     }
 }
